00184e97e6d5912d2ee929db07b375d19e56000d
Ahmed Al Imran <ahmedfelah@gmail.com>
2020-03-22 13:17:19 +0300

    remove pods file

--- Pods/Target Support Files/Pods-e-learning/Pods-e-learning-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_e_learning : NSObject
-@end
-@implementation PodsDummy_Pods_e_learning
-@end
--- Pods/Target Support Files/Pods-e-learningTests/Pods-e-learningTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_e_learningTests : NSObject
-@end
-@implementation PodsDummy_Pods_e_learningTests
-@end
